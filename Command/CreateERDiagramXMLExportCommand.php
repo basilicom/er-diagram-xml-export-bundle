8577f3d2dcb44b3ad85ac6244f5c5b3eb6a58a65
Hendrik Hofmann <hendrik.hofmann@basilicom.de>
2021-01-19 11:23:19 +0100

    Added FieldCollections and ObjectBricks

--- Command/CreateERDiagramXMLExportCommand.php
+++ Command/CreateERDiagramXMLExportCommand.php
@@ -4,12 +4,15 @@
 namespace ERDiagramXMLExportBundle\Command;
 
 use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\ClassDefinition\Data\Fieldcollections as FieldCollections;
+use Pimcore\Model\DataObject\ClassDefinition\Data\Objectbricks as ObjectBricks;
+use Pimcore\Model\DataObject\ClassDefinition\Listing as ClassDefinitionListing;
+use Pimcore\Model\DataObject\Fieldcollection\Definition\Listing as FieldCollectionListing;
+use Pimcore\Model\DataObject\Objectbrick\Definition\Listing as ObjectBrickListing;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Pimcore\Model\DataObject\ClassDefinition\Listing as ClassDefinitionListing;
-use \Pimcore\Model\DataObject\Fieldcollection\Definition\Listing as FieldCollectionListing;
-use Pimcore\Model\DataObject\ClassDefinition\Data\Fieldcollections as FieldCollections;
+
 class CreateERDiagramXMLExportCommand extends Command
 {
     protected static $defaultName = 'basilicom:create-er-diagram-xml-export';
@@ -21,8 +24,12 @@ class CreateERDiagramXMLExportCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $result = new GraphMLWriter(
+            $this->getClassDefinitionData(),
+            $this->getFieldCollectionsData(),
+            $this->getObjectBricksData()
 
-        $result = new GraphMLWriter($this->getClassDefinitionData(), $this->getFieldCollectionsData());
+        );
         $result->output();
 
         return 0;
@@ -36,15 +43,16 @@ class CreateERDiagramXMLExportCommand extends Command
         $classDefinitionData = [];
 
         foreach ($classDefinitions as $classDefinition) {
-
             $fieldDefinitions = $classDefinition->getFieldDefinitions();
+
 
             $data = [
                 'id' => $classDefinition->getId(),
                 'name' => $classDefinition->getName(),
                 'fields' => $this->processFieldDefinitions($fieldDefinitions),
                 'relatedClasses' => $this->getRelatedClasses($fieldDefinitions),
-
+                'relatedFieldCollections' => $this->getRelatedFieldCollections($fieldDefinitions),
+                'relatedObjectBricks' => $this->getRelatedObjectBricks($fieldDefinitions),
             ];
 
             array_push($classDefinitionData, $data);
@@ -52,7 +60,6 @@ class CreateERDiagramXMLExportCommand extends Command
 
 
         return $classDefinitionData;
-
     }
 
 
@@ -69,8 +76,6 @@ class CreateERDiagramXMLExportCommand extends Command
                     array_push($relatedClasses, [$fieldType => $class['classes']]);
                 }
             }
-
-
         }
 
         return $relatedClasses;
@@ -86,22 +91,19 @@ class CreateERDiagramXMLExportCommand extends Command
 
             if (strpos($fieldType, 'Relation') == false) {
                 $fields = [
-                    $fieldDefinition->getName() => $fieldType
+                    $fieldDefinition->getName() => $fieldType,
                 ];
                 if ($fieldDefinition instanceof FieldCollections) {
-
                     $allowedTypes = [];
 
                     foreach ($fieldDefinition->getAllowedTypes() as $allowedType) {
                         array_push($allowedTypes, $allowedType);
                     }
                     $fields = [
-                        $fieldDefinition->getName() => $allowedTypes
+                        $fieldDefinition->getName() => $allowedTypes,
                     ];
                 }
-
                 array_push($data, $fields);
-
             }
         }
 
@@ -110,29 +112,77 @@ class CreateERDiagramXMLExportCommand extends Command
 
     private function getFieldCollectionsData(): array
     {
-      $fieldCollectionData = [];
+        $fieldCollectionData = [];
 
-      $fieldCollectionListing = new FieldCollectionListing();
-      $fieldCollections = $fieldCollectionListing->load();
+        $fieldCollectionListing = new FieldCollectionListing();
+        $fieldCollections = $fieldCollectionListing->load();
 
+        foreach ($fieldCollections as $fieldCollection) {
+            $data['fieldCollection'] = [
 
+                'name' => $fieldCollection->getKey(),
+                'fields' => $this->processFieldDefinitions($fieldCollection->getFieldDefinitions()),
 
-      foreach ($fieldCollections as $fieldCollection) {
+            ];
+            array_push($fieldCollectionData, $data);
+        }
 
-    //      dump($fieldCollection);
-          $data = [
-
-              'name' => $fieldCollection->getKey(),
-              'fields' => $this->processFieldDefinitions($fieldCollection->getFieldDefinitions())
-
-          ];
-          array_push($fieldCollectionData, $data);
-
-      }
-
-     // dump(($fieldCollectionData));
-      return $fieldCollectionData;
+        return $fieldCollectionData;
     }
 
-    //TODO  ObjectBricks laden (abbilden)
+    private function getRelatedFieldCollections($fieldDefinitions): array
+    {
+        $data = [];
+
+        foreach ($fieldDefinitions as $fieldDefinition) {
+            if ($fieldDefinition instanceof FieldCollections) {
+                foreach ($fieldDefinition->getAllowedTypes() as $allowedType => $name) {
+                    array_push($data, $name);
+                }
+            }
+
+        }
+
+        return $data;
+    }
+
+    private function getRelatedObjectBricks($fieldDefinitions): array
+    {
+        $data = [];
+
+        foreach ($fieldDefinitions as $fieldDefinition) {
+            if ($fieldDefinition instanceof ObjectBricks) {
+                foreach ($fieldDefinition->getAllowedTypes() as $allowedType => $name) {
+                    array_push($data, $name);
+                }
+            }
+
+        }
+
+        return $data;
+    }
+
+    private function getObjectBricksData(): array
+    {
+        $objectBricksData = [];
+
+        $objectBricksListing = new ObjectBrickListing();
+        $objectBricks = $objectBricksListing->load();
+
+        foreach ($objectBricks as $objectBrick) {
+            $data['objectBrick'] = [
+
+                'name' => $objectBrick->getKey(),
+                'fields' => $this->processFieldDefinitions($objectBrick->getFieldDefinitions()),
+
+            ];
+            array_push($objectBricksData, $data);
+        }
+
+        return $objectBricksData;
+    }
+
+
+
+
 }
--- Command/GraphMLWriter.php
+++ Command/GraphMLWriter.php
@@ -7,28 +7,29 @@ use Spatie\ArrayToXml\ArrayToXml;
 
 class GraphMLWriter
 {
-
-    private $classDefinitions = [];
-    private $fieldCollections = [];
-    private $xmlOutput = '';
-    private $actualNodeId = 0;
-    private $actualEdgeId = 0;
+    private array $classDefinitions = [];
+    private array $fieldCollections = [];
+    private array $objectBricks = [];
+    private string $xmlOutput = '';
+    private int $actualEdgeId = 0;
+    private int $actualBoxSize = 0;
 
     /**
      * GraphMLWriter constructor.
      *
      * @param array $classDefinitions
      * @param array $fieldCollections
+     * @param array $objectBricks
      */
-    public function __construct(array $classDefinitions, array $fieldCollections)
+    public function __construct(array $classDefinitions, array $fieldCollections, array $objectBricks)
     {
         $this->classDefinitions = $classDefinitions;
         $this->fieldCollections = $fieldCollections;
+        $this->objectBricks = $objectBricks;
     }
 
     public function output()
     {
-        // dump($this->data);
         $this->createHeader();
 
         $this->createNodesAndEdges();
@@ -57,33 +58,58 @@ class GraphMLWriter
         foreach ($this->classDefinitions as $classDefinition) {
             $this->createNode($classDefinition);
 
-            // Wenn die Klasse Relations hat
             $relatedClasses = $classDefinition['relatedClasses'];
+            $relatedFieldCollections = $classDefinition['relatedFieldCollections'];
+            $relatedObjectBricks = $classDefinition['relatedObjectBricks'];
+
+            $parentClass = $classDefinition['name'];
 
             if (!empty($relatedClasses)) {
-                $parentClass = $classDefinition['name'];
-
                 foreach ($relatedClasses as $class) {
                     foreach ($class as $relationType => $className) {
-                        $this->createEdge($parentClass, $className);
+                        $this->createEdge($parentClass, $className, $relationType);
                     }
                 }
             }
+            if (!empty($relatedFieldCollections)) {
+                foreach ($relatedFieldCollections as $relatedFieldCollection => $name) {
+                    $this->createEdge($parentClass, $name, 'onetomany');
+                }
+            }
+
+            if (!empty($relatedObjectBricks)) {
+                foreach ($relatedObjectBricks as $relatedObjectBrick => $name) {
+                    $this->createEdge($parentClass, $name, '');
+                }
+            }
         }
-        foreach ($this->fieldCollections as $fieldCollection) {
-            $this->createNode($fieldCollection, true);
+
+        foreach ($this->fieldCollections as $fieldCollectionData) {
+            foreach ($fieldCollectionData as $fieldCollection) {
+                $this->createNode($fieldCollection, true);
+            }
+        }
+
+        foreach ($this->objectBricks as $objectBricksData) {
+            foreach ($objectBricksData as $objectBrick) {
+                $this->createNode($objectBrick, false, true);
+            }
         }
     }
 
 
-    private function createNode(array $entry, bool $isFieldCollection = false )
+    private function createNode(array $entry, bool $isFieldCollection = false, bool $isObjecktBrick = false)
     {
         $className = $entry['name'];
         $fillColor = '#E8EEF7';
         if ($isFieldCollection) {
-            $fillColor = '#f7f0e8';
+            $fillColor = '#F7F0E8';
+        }
+        if ($isObjecktBrick) {
+            $fillColor = '#F7E8E8';
         }
 
+        $attributes = $this->createAttributes($entry);
 
         /*
          * Sadly i cant use Spatie\ArrayToXml\ArrayToXml here because it's not possible to set an array for the _value
@@ -93,7 +119,7 @@ class GraphMLWriter
         $nodeContent = '<node id="%s">
           <data key="nodegraphics">
             <y:GenericNode configuration="com.yworks.entityRelationship.big_entity">
-              <y:Geometry height="150.0" width="160.0" />
+              <y:Geometry height="%d" width="160" />
               <y:Fill color="#E8EEF7" color2="#B7C9E3" transparent="false"/>
               <y:NodeLabel alignment="center" autoSizePolicy="content" backgroundColor="%s" configuration="com.yworks.entityRelationship.label.name"  horizontalTextPosition="center" modelName="internal" modelPosition="t" textColor="#000000" verticalTextPosition="bottom" visible="true"  >%s</y:NodeLabel>
               %s
@@ -101,11 +127,10 @@ class GraphMLWriter
           </data>
         </node>';
 
-        $nodeContent = sprintf($nodeContent, $className, $fillColor, $className, $this->createAttributes($entry));
+
+        $nodeContent = sprintf($nodeContent, $className, $this->actualBoxSize, $fillColor, $className, $attributes);
 
         $this->xmlOutput .= $nodeContent;
-
-        $this->actualNodeId += 1;
     }
 
     private function createAttributes($entry): string
@@ -114,61 +139,83 @@ class GraphMLWriter
         $attributesString = '';
 
         if (!empty($fields)) {
+            $this->actualBoxSize = 60 + count($fields) * 30;
+
             foreach ($fields as $field) {
                 foreach ($field as $fieldname => $fieldtype) {
-                    if (!is_array($fieldtype)){
+                    if (!is_array($fieldtype)) {
                         $attributesString .= $fieldname . ': ' . $fieldtype . PHP_EOL;
                     }
-
                     if (is_array($fieldtype)) {
                         $allowedTypes = '';
                         foreach ($fieldtype as $index => $allowedType) {
-                            $allowedTypes .= $allowedType . ' | ' ;
+                            $allowedTypes .= $allowedType . ' | ';
                         }
-                        $allowedTypes = substr($allowedTypes,0, -3);
+
+                        $allowedTypes = substr($allowedTypes, 0, -3);
                         $attributesString .= $fieldname . ': ' . $allowedTypes . PHP_EOL;
                     }
                 }
             }
         }
 
-      $rootElement = [
-          'rootElementName' => 'y:NodeLabel',
-          '_attributes' => [
-              'alignement' => 'left',
-              'autoSizePolicy' => 'content',
-              'horizontalTextPosition' => 'center',
-              'verticalTextPosition' => 'top',
-              'visible' => 'true',
-              'clipContent' => 'true',
-              'hasDetailsColor' => 'false',
-              'omitDetails' => 'false',
-              'modelName' => 'internal',
-              'modelPosition' => 'c',
-              'configuration' => 'com.yworks.entityRelationship.label.attributes',
-          ],
 
-      ];
+        $rootElement = [
+            'rootElementName' => 'y:NodeLabel',
+            '_attributes' => [
+                'alignement' => 'left',
+                'autoSizePolicy' => 'content',
+                'horizontalTextPosition' => 'center',
+                'verticalTextPosition' => 'top',
+                'visible' => 'true',
+                'clipContent' => 'true',
+                'hasDetailsColor' => 'false',
+                'omitDetails' => 'false',
+                'modelName' => 'internal',
+                'modelPosition' => 'c',
+                'configuration' => 'com.yworks.entityRelationship.label.attributes',
+            ],
 
-      $attributes = [
-          '_value' => $attributesString
-      ];
+        ];
 
-      $arrayToXml = new ArrayToXml($attributes, $rootElement);
+        $attributes = [
+            '_value' => $attributesString,
+        ];
 
-      return $arrayToXml->dropXmlDeclaration()->prettify()->toXml();
+        $arrayToXml = new ArrayToXml($attributes, $rootElement);
+
+        return $arrayToXml->dropXmlDeclaration()->prettify()->toXml();
     }
 
-    private function createEdge($source, $target)
+    private function createEdge($source, $target, $relationType = '')
     {
+        if (strpos(strtolower($relationType), 'manytomany') !== false) {
+            $sourceArrowType = 'crows_foot_many';
+            $targetArrowType = 'crows_foot_many';
+        }
+        if (strpos(strtolower($relationType), 'onetomany') !== false) {
+            $sourceArrowType = 'none';
+            $targetArrowType = 'crows_foot_many';
+        }
+        if (strpos(strtolower($relationType), 'manytoone') !== false) {
+            $sourceArrowType = 'crows_foot_many';
+            $targetArrowType = 'none';
+        }
+        if ($relationType == '') {
+            $sourceArrowType = 'none';
+            $targetArrowType = 'none';
+        }
+
         $edgeContent = "
         <edge id='%s' source='%s' target='%s'>
             <data key='edgegraphics'>
-                 <y:PolyLineEdge></y:PolyLineEdge>
+                 <y:PolyLineEdge>
+                           <y:Arrows source='%s' target='%s'/>
+                 </y:PolyLineEdge>
             </data>
         </edge>";
 
-        $edgeContent = sprintf($edgeContent, $this->actualEdgeId, $source, $target);
+        $edgeContent = sprintf($edgeContent, $this->actualEdgeId, $source, $target, $sourceArrowType, $targetArrowType);
         $this->actualEdgeId += 1;
 
         $this->xmlOutput .= $edgeContent;
@@ -178,7 +225,6 @@ class GraphMLWriter
     /** BASTODO Ordentlich bauen */
     private function writeToFile()
     {
-
         $file = __DIR__ . '/' . 'output.graphml';
         file_put_contents($file, $this->xmlOutput);
     }
