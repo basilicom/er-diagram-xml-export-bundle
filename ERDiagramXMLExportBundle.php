8f7b490651a5583dd2a4df5741d535b30ee57f4d
Hendrik Hofmann <hendrik.hofmann@basilicom.de>
2021-01-25 15:25:08 +0100

    Adopted folder structure accroding to pimcore bundle guidelines

--- ERDiagramXMLExportBundle.php
+++ ERDiagramXMLExportBundle.php
@@ -6,5 +6,11 @@ use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
 class ERDiagramXMLExportBundle extends AbstractPimcoreBundle
 {
-
+    /**
+     * @inheritDoc
+     */
+    public function getDescription()
+    {
+        return 'Provides an YED Compliant XML Represantation of the Pimcore Class Structure';
+    }
 }
